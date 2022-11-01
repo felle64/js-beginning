e9685b18db3c5d9ba896d677ce0aac70f09a8a8d
felle64 <felle641@gmail.com>
2022-11-01 15:11:07 +0100

    minor changes

---
+++ Codeacademy/script.js
@@ -0,0 +1,13 @@
+// Temperature in Kelvin
+const kelvin = 0;
+// Temperature converstion to celsius
+const celsius = kelvin - 273;
+// Temperature converstion to fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+//Rounds fahrenheit to a whole number
+fahrenheit = Math.floor(fahrenheit);
+// Log temperature in celsius to console
+console.log(`The temperature is ${celsius} degrees Celsius.`);
+// Log temperature in fahrenheit to console
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
