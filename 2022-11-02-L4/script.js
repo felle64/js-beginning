e1501bf81b4b5deb0aebd68997a32d508e765997
felle64 <felle641@gmail.com>
2022-11-10 12:05:15 +0100

    yes

---
+++ 2022-11-02-L4/script.js
@@ -0,0 +1,15 @@
+//const is_odd = (a) => {
+  //  if (a % 2) {
+        // a is odd
+    //    console.log(a + " is odd")
+      //  return true;
+   // } else {
+        // a is even
+     // return false;
+        
+   // }
+//}
+
+//is_odd(2);
+
+Math.round(Math.random() * 10) +1;
