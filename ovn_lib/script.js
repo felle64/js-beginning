d58b6b725b82750660b1380dc69d796ab450d9d5
felle64 <felle641@gmail.com>
2022-12-06 23:11:54 +0100

    printing hardcoded msg

--- ovn_lib/script.js
+++ ovn_lib/script.js
@@ -1,4 +1,13 @@
 import Books from "./books.js";
 
 let books = new Books("Livet på spel", "Heaton", 305, "loaned out");
-console.log(books);
+console.log(books.printBooks());
+
+let para = document.createElement("p");
+let box = document.getElementById("box");
+
+function printToPage(){
+    box.innerHTML = books.printBooks();
+}
+
+printToPage();
