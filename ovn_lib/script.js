7c3304c5bb5de792c197bd9d06fcafd21ad6ed77
felle64 <felle641gmail.com>
2022-12-06 23:59:43 +0100

    testing testing

--- ovn_lib/script.js
+++ ovn_lib/script.js
@@ -3,11 +3,14 @@ import Books from "./books.js";
 let books = new Books("Livet på spel", "Heaton", 305, "loaned out");
 console.log(books.printBooks());
 
-let para = document.createElement("p");
+let para = document.createElement("h2");
 let box = document.getElementById("box");
+para.textContent = "HELLO";
 
 function printToPage(){
-    box.innerHTML = books.printBooks();
+    box.innerHTML = books.printBooks();   
+   
 }
 
 printToPage();
+console.log(para);
